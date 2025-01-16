994bb677df7d0165fdcc6a549b4a55985971a016
Victoria Hu <xintonghu123@gmail.com>
2025-01-15 23:55:54 -0500

    trying stuff yippee

--- client/src/components/modules/NavBar.jsx
+++ client/src/components/modules/NavBar.jsx
@@ -10,21 +10,27 @@ import "./NavBar.css";
 const NavBar = (props) => {
   return (
     <nav className="NavBar-container">
-      <div className="NavBar-title u-inlineBlock">Catbook</div>
+      <div className="NavBar-title u-inlineBlock">StyleSnap</div>
       <div className="NavBar-linkContainer u-inlineBlock">
         <Link to="/" className="NavBar-link">
           Home
         </Link>
         {props.userId && (
-          <Link to={`/profile/${props.userId}`} className="NavBar-link u-inlineBlock">
+          <Link
+            to={`/profile/${props.userId}`}
+            className="NavBar-link u-inlineBlock"
+          >
             Profile
           </Link>
         )}
         <Link to="/chat/" className="NavBar-link u-inlineBlock">
-          Chat
+          Delete
         </Link>
         {props.userId ? (
-          <button className="NavBar-link NavBar-login u-inlineBlock" onClick={props.handleLogout}>
+          <button
+            className="NavBar-link NavBar-login u-inlineBlock"
+            onClick={props.handleLogout}
+          >
             Sign out
           </button>
         ) : (
@@ -32,7 +38,9 @@ const NavBar = (props) => {
             text="signin_with"
             onSuccess={props.handleLogin}
             onFailure={(err) => console.log(err)}
-            containerProps= {{'className': "NavBar-link NavBar-login u-inlineBlock"}}
+            containerProps={{
+              className: "NavBar-link NavBar-login u-inlineBlock",
+            }}
           />
         )}
       </div>
