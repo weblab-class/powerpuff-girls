9cb8b99c98e7e2430dc27bb02ee1378db22d6843
Mihika Dusad <mihikadusad@dhcp-10-29-139-28.dyn.MIT.EDU>
2025-01-20 18:18:17 -0500

    added blue buttons

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import { get } from "../../utilities";
+import "../../tailwind.css"
 import "./Feed.css";
 
 const Feed = () => {
@@ -103,7 +104,7 @@ const Feed = () => {
       {<SearchFeed filterFeed={filterFeed} />}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       onClick={clearSearch}>
-        Clear search
+        Clear Search
       </button>
 
       <div className="horizontal-spread">
