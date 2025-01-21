fffcd33414c475a330dc98f39bb114e6ddcaca81
Mihika Dusad <mihikadusad@dhcp-10-29-139-28.dyn.MIT.EDU>
2025-01-21 17:20:59 -0500

    final new_ui

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,9 +9,9 @@ import { get, post } from "../utilities";
 import { Outlet } from "react-router-dom";
 
 // to use styles, import the necessary CSS files
-import "../utilities.css";
-import "./App.css";
-
+// import "../utilities.css";
+//import "./App.css";
+import "../index.css";
 /**
  * Define the "App" component as a function.
  */
