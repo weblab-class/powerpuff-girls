e67476e8aca70bca0bb2c7574b98b2618bc7d6de
Michelle Wei <michelle@dhcp-10-29-215-230.dyn.MIT.EDU>
2025-01-23 23:39:49 -0500

    updated saved page

--- client/src/components/pages/Saved.jsx
+++ client/src/components/pages/Saved.jsx
@@ -16,7 +16,6 @@ const Saved = () => {
   let props = useOutletContext();
   const [stories, setStories] = useState([]);
   const [filterStories, setFilterStories] = useState([]);
-  const [showing, setShowing] = useState(0);
   const [popUp, setPopUp] = useState(-1); //-1 means nothing, otherwise give index of card
 
   // called when the "Feed" component "mounts", i.e.
@@ -34,7 +33,6 @@ const Saved = () => {
         setFilterStories(reversedStoryObjs);
       });
     });
-    setShowing(0);
   }, []);
 
   // this gets called when the user pushes "Submit", so their
@@ -43,7 +41,7 @@ const Saved = () => {
     setStories([storyObj].concat(stories));
   };
 
-  const filterFeed = (query) => {
+  /*const filterFeed = (query) => {
     if (query === "") {
       setFilterStories(stories);
       setShowing(0);
@@ -73,16 +71,11 @@ const Saved = () => {
     if (showing * 4 - 4 >= 0) {
       setShowing(showing - 1);
     }
-  };
+  };*/
 
   let storiesList = null;
   if (filterStories.length !== 0) {
-    console.log(filterStories);
-    let fourStories = filterStories.slice(
-      showing * 4,
-      Math.min(showing * 4 + 4, filterStories.length)
-    );
-    storiesList = fourStories.map((storyObj) => (
+    storiesList = filterStories.map((storyObj) => (
       <button onClick={() => navigate(`/post/${storyObj._id}`)}>
         <Card
           key={`Card_${storyObj._id}`}
@@ -97,24 +90,16 @@ const Saved = () => {
         />
       </button>
     ));
-  } else if (filterStories.length === 0 && stories.length !== 0) {
-    console.log(filterStories);
-    storiesList = <div>Nothing matches your search...</div>;
   } else {
-    storiesList = <div>Nothing in your feed!</div>;
+    storiesList = <div>Nothing Saved!</div>;
   }
   return (
     <>
       <div className="container mx-auto px-4 pt-24 pb-12">
         <div>
-          <button onClick={goLeft} className="arrow-button arrow-left"></button>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {storiesList}
           </div>
-          <button
-            onClick={goRight}
-            className="arrow-button arrow-right"
-          ></button>
         </div>
       </div>
     </>
