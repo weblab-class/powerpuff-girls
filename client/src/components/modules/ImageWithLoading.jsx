25e269b165dfda5c772c0eced3d35497f17e9386
Michelle Wei <michelle@dhcp-10-29-215-230.dyn.MIT.EDU>
2025-01-29 02:03:55 -0500

    small UI changes

--- client/src/components/modules/ImageWithLoading.jsx
+++ client/src/components/modules/ImageWithLoading.jsx
@@ -15,26 +15,28 @@ const ImageWithLoading = ({ base64Image }) => {
       {loading ? (
         <div className="flex items-center justify-center m-4">
           Loading your AI color palette analysis!
-          <svg
-            className="h-8 w-8 animate-spin text-blue-500"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8v8H4z"
-            ></path>
-          </svg>
+          <span className="ml-2">
+            <svg
+              className="h-8 w-8 animate-spin text-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v8H4z"
+              ></path>
+            </svg>
+          </span>
         </div>
       ) : (
         <div className="flex items-center space-x-4 bg-white w-full">
