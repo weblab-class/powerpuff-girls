8a519702f4214672347d289b11ef1c30646c82c4
Victoria Hu <xintonghu123@gmail.com>
2025-01-28 22:54:28 -0500

    bash script 3

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+ls -a /opt/render/project/src
+ls -a /opt/render/project/src/.venv
+ls -a /opt/render/project/src/.venv/bin
 source /opt/render/project/src/.venv/bin/activate
-
-echo "virtual environment: $VIRTUAL_ENV"
 
 pip list
